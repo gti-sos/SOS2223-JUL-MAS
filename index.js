13b3111aa77b47797a390cec620d6ffbf0b1ba84
Miguel Angel Salinas <masm.htc@gmail.com>
2023-07-09 20:46:26 +0200

    index change

--- index.js
+++ index.js
@@ -8,26 +8,14 @@ app.use(express.json());
 app.use(cors());
 import {handler} from "./frontend/build/handler.js"
 console.log("b");
-//var backend_MAS = require("./backend/index_MAS");
 
-import {loadBackend_LMP} from "./backend/index_LMP.js";
-//import {loadBackend_LMPv2} from "./backend/index_LMPv2.js";
-import {loadBackend_LMPv3} from "./backend/index_LMPv3.js";
 import {loadBackend_MASv3} from "./backend/index_MASv3.js";
 import {loadBackend_MAS} from "./backend/index_MAS.js";
 import {loadBackend_MASv2} from "./backend/index_MASv2.js";
-import {loadBackend_CGM} from "./backend/index_CGM.js";
-import {loadBackend_CGMv2} from "./backend/index_CGMv2.js";
 console.log("c");
-//var backend_CGM = require("./backend/index_CGM");
 loadBackend_MAS(app);
 loadBackend_MASv2(app);
-//loadBackend_LMPv2(app);
 loadBackend_MASv3(app);
-loadBackend_LMPv3(app);
-loadBackend_LMP(app);
-loadBackend_CGM(app);
-loadBackend_CGMv2(app);
 console.log("d");
 
 app.use(handler);
