e4c0009f7924eee0e4d4f29f55cb9568cd8ea417
Miguel Angel Salinas <masm.htc@gmail.com>
2023-07-10 23:17:36 +0200

    change estetico

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 console.log("a");
 import express from "express";
 import cors from 'cors';
-//min change
 var port = process.env.PORT || 12345;
 var app = express();
 app.use(express.json());
 app.use(cors());
 import {handler} from "./frontend/build/handler.js"
 console.log("b");
-
 import {loadBackend_MASv3} from "./backend/index_MASv3.js";
 import {loadBackend_MAS} from "./backend/index_MAS.js";
 import {loadBackend_MASv2} from "./backend/index_MASv2.js";
-
 import {loadBackend_LMPv2} from "./backend/index_LMPv2.js";
 console.log("c");
 loadBackend_MAS(app);
@@ -22,8 +19,6 @@ loadBackend_LMPv2(app);
 console.log("d");
 
 app.use(handler);
-
-
 app.listen(port,() =>{
   console.log("e");console.log(`Servidor corriendo en el puerto: ${port}`);
 });
